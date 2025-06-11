ba6cce5e09ceab94fb1e0accf18689ff7ae99730
Jimena Ruiz <jyellow363@gmail.com>
2025-06-11 18:39:00 -0500

    Se agrega estructura inicial del proyecto con dependencias.

---
+++ src/main/java/com/lmp/unmsm/MatriculaUnmsmApplication.java
@@ -0,0 +1,13 @@
+package com.lmp.unmsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaUnmsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaUnmsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmp/unmsm/MatriculaUnmsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmp.unmsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaUnmsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
